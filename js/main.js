b7c24b46fbab3134623b674fc2087c6fbae60212
Dharma Sanjyal <rajsan44@gmail.com>
2017-11-03 11:35:16 -0700

    third commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+function changeImage() {
+	document.getElementById('picture').src ='img/org.jpg';
+}
+document.getElementById('btn').addEventListener('click', changeImage, false);
+function originalImage(){
+	document.getElementById('picture').src = 'img/orange.png';
+}
+document.getElementById('btn2').addEventListener('click', originalImage, false);
 
